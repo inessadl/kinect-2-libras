7c9c0117bc157a468af9346670a834ae5f682852
Inessa Luerce <idluerce@inf.ufpel.edu.br>
2016-02-23 10:27:24 -0300

    line ending error fixed

--- Kinect2Libras/Kinect2Libras/MainPage.xaml.cs
+++ Kinect2Libras/Kinect2Libras/MainPage.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System;
+﻿using LightBuzz.Vitruvius.FingerTracking;
+using Microsoft.Kinect;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Kinect2Libras
